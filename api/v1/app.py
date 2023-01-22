042e27b908af683aab4369573e0585e00fad41cd
AbdiAbader <abdiprocode@gmail.com>
2023-01-22 10:24:50 +0300

    api recreated

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module contains the Flask app"""
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -9,12 +9,21 @@ from os import getenv
 app = Flask(__name__)
 
 @app.teardown_appcontext
-def teardown_db(self):
-    """Closes the storage on teardown"""
+def tear_down(self):
+    '''
+    close query after each session
+    '''
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    '''
+    return JSON formatted 404 status code response
+    '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
     app.register_blueprint(app_views)
     app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
             port=int(getenv('HBNB_API_PORT', 5000), threaded=True))
-            
