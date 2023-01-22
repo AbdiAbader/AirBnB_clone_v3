f5268ebbec957c20470789bb994368781553483a
AbdiAbader <abdiprocode@gmail.com>
2023-01-22 10:19:12 +0300

    api recreated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,34 +1,11 @@
 #!/usr/bin/python3
-'''
-    flask with general routes
-    routes:
-        /status:    display "status":"OK"
-        /stats:     dispaly total for all classes
-'''
+
+"""This module contains the Flask app"""
+
+from flask import Flask, jsonify, request, abort
 from api.v1.views import app_views
-from flask import jsonify
-from models import storage
 
-
-@app_views.route("/status")
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    '''
-        return JSON of OK status
-    '''
+    """Returns a JSON status"""
     return jsonify({'status': 'OK'})
-
-
-@app_views.route("/stats")
-def storage_counts():
-    '''
-        return counts of all classes in storage
-    '''
-    cls_counts = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
-    }
-    return jsonify(cls_counts)
