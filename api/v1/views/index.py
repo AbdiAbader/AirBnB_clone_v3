d349e3d71e77bba07dc859438561410b4cb7e8f7
AbdiAbader <abdiprocode@gmail.com>
2023-01-22 11:18:21 +0300

    index updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,21 +4,25 @@
 
 from api.v1.views import app_views
 from flask import jsonify
-
+from models import storage
 
 @app_views.route('/status')
 def status():
     """ Return status """
     return jsonify({"status": "OK"})
 
-@app_views.route('/stats')
-def stats():
-    """ Return stats """
-    from models import storage
-    return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")})
-                    
+
+@app_views.route("/stats")
+def storage_counts():
+    '''
+        return counts of all classes in storage
+    '''
+    cls_counts = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(cls_counts)
