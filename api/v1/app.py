c7b2e7c361c073bbf6c5a71f96de7aa2ac017d2e
AbdiAbader <abdiprocode@gmail.com>
2023-01-22 10:26:24 +0300

    api recreated

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,6 +7,7 @@ from os import getenv
 
 
 app = Flask(__name__)
+app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def tear_down(self):
@@ -15,13 +16,6 @@ def tear_down(self):
     '''
     storage.close()
 
-
-@app.errorhandler(404)
-def not_found(error):
-    '''
-    return JSON formatted 404 status code response
-    '''
-    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
     app.register_blueprint(app_views)
