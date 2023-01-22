ef65c24c2ed8453aefbbd08561a2eb5a6eb3c8da
AbdiAbader <abdiprocode@gmail.com>
2023-01-22 10:31:25 +0300

    api recreated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,9 @@
 from flask import Flask, jsonify, request, abort
 from api.v1.views import app_views
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route("/status")
 def status():
-    """Returns a JSON status"""
+    '''
+        return JSON of OK status
+    '''
     return jsonify({'status': 'OK'})
