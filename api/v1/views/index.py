608db423044d2a53033f74400672a3bfcb0749c8
AbdiAbader <abdiprocode@gmail.com>
2023-01-22 11:16:24 +0300

    index updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-'''
-    flask with general routes
-    routes:
-        /status:    display "status":"OK"
-        /stats:     dispaly total for all classes
-'''
+""" Index file """
+
+
 from api.v1.views import app_views
 from flask import jsonify
-from models import storage
 
 
-@app_views.route("/status")
+@app_views.route('/status')
 def status():
-    '''
-        return JSON of OK status
-    '''
-    return jsonify({'status': 'OK'})
+    """ Return status """
+    return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def stats():
+    """ Return stats """
+    from models import storage
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
+                    
