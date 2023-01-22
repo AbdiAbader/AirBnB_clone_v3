d3ddc7b3bbbdcfe5a13eb740ed654abc6186ef28
AbdiAbader <abdiprocode@gmail.com>
2023-01-22 12:48:28 +0300

    all

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,33 +1,26 @@
 #!/usr/bin/python3
-"""index.py to connect to API"""
+""" index file """
+
+
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify
+from flask import jsonify
 from models import storage
 
 
-hbnbText = {
-    "amenities": "Amenity",
-    "cities": "City",
-    "places": "Place",
-    "reviews": "Review",
-    "states": "State",
-    "users": "User"
-}
-
-
-@app_views.route('/status', strict_slashes=False)
-def hbnbStatus():
-    """hbnbStatus"""
+@app_views.route("/status")
+def status():
+    '''
+        return status of API
+    '''
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', strict_slashes=False)
-def hbnbStats():
-    """hbnbStats"""
-    return_dict = {}
-    for key, value in hbnbText.items():
-        return_dict[key] = storage.count(value)
-    return jsonify(return_dict)
-
-if __name__ == "__main__":
-    pass
+@app_views.route("/stats")
+def storage_counts():
+    """ returns the number of each objects by type """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
--- models/__init__.py
+++ models/__init__.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""
-initialize the models package
-"""
+""" initialize the models package """
+
 
 from os import getenv
+from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 
-# DBSTORAGE
-if getenv('HBNB_TYPE_STORAGE') == 'db':
-    from models.engine.db_storage import DBStorage
+
+if getenv("HBNB_TYPE_STORAGE") == "db":
     storage = DBStorage()
-# FILESTORAGE
 else:
-    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 storage.reload()
