2c7521152887fdd10d496d907d7d55c1228251b6
AbdiAbader <abdiprocode@gmail.com>
2023-01-22 10:28:53 +0300

    api recreated

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-"""This module contains the Flask app"""
-from flask import Flask, jsonify, request, abort, make_response
+'''
+    app for registering blueprint and starting flask
+'''
+from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 
 app = Flask(__name__)
+CORS(app, origins="0.0.0.0")
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def tear_down(self):
@@ -17,7 +22,14 @@ def tear_down(self):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    '''
+    return JSON formatted 404 status code response
+    '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
 if __name__ == "__main__":
-    app.register_blueprint(app_views)
-    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(getenv('HBNB_API_PORT', 5000), threaded=True))
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
+            port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
