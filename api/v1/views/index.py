d5e3fcbe00645fc9a07628224762bbc63b550d27
AbdiAbader <abdiprocode@gmail.com>
2023-01-23 12:09:31 +0300

    index file updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-""" index file """
-
-
+'''
+    flask with general routes
+    routes:
+        /status:    display "status":"OK"
+        /stats:     dispaly total for all classes
+'''
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
@@ -10,19 +13,22 @@ from models import storage
 @app_views.route("/status")
 def status():
     '''
-        return status of API
+        return JSON of OK status
     '''
-    return jsonify({"status": "OK"})
+    return jsonify({'status': 'OK'})
 
 
 @app_views.route("/stats")
 def storage_counts():
-    """ returns the number of each objects by type """
-    count = {}
-    count["amenities"] = storage.count("Amenity")
-    count["cities"] = storage.count("City")
-    count["places"] = storage.count("Place")
-    count["reviews"] = storage.count("Review")
-    count["states"] = storage.count("State")
-    count["users"] = storage.count("User")
-    return jsonify(count)
+    '''
+        return counts of all classes in storage
+    '''
+    cls_counts = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(cls_counts)
