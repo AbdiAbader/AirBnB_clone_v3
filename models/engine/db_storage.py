ac591b9881b988073cd0e4ba2fbea71547691f8a
AbdiAbader <abdiprocode@gmail.com>
2023-01-23 14:29:59 +0300

    all

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -86,11 +86,14 @@ class DBStorage:
         if cls and id:
             return self.__session.query(cls).filter_by(id=id).first()
         return None
+
     def count(self, cls=None):
-        """Returns the number of objects in storage matching the given class name"""
+        """Count number of objects in storage"""
+        total = 0
         if type(cls) == str and cls in name2class:
-            return self.__session.query(cls).count()
+            cls = name2class[cls]
+            total = self.__session.query(cls).count()
         elif cls is None:
-            return self.__session.query(User).count() + self.__session.query(State).count() + self.__session.query(City).count() + self.__session.query(Amenity).count() + self.__session.query(Place).count() + self.__session.query(Review).count()
-        else:
-            return 0
+            for cls in name2class.values():
+                total += self.__session.query(cls).count()
+        return total
